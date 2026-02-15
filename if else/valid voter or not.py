e5c8c073da8152b2c1e0fb836199c61962625e77
prithvi-techie <prithvi.techie@gmail.com>
2026-02-15 17:26:52 +0530

    loops

---
+++ if else/valid voter or not.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+if age >=18:
+    print(f"Hello {name} you are a valid voter")
+else:
+    print("You are not eligible for voting")
---
+++ loops/for loop ques/ques6.py
@@ -0,0 +1,6 @@
+# print sum up to n number
+n = int(input("Enter your number: "))
+sum =0
+for i in range(1,n+1):
+    sum = sum + i
+print(sum)
