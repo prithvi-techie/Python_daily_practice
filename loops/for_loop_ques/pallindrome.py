3691d6098856de80aba8fe963cb7b1acdc68f4ce
prithvi-techie <prithvi.techie@gmail.com>
2026-02-17 22:23:53 +0530

    check string is pallindrome or not

--- loops/for_loop_ques/pallindrome.py
+++ loops/for_loop_ques/pallindrome.py
@@ -1,6 +1,12 @@
 #check string is pallindrome or not
 # sidha ulta kahi se bhi padho same aata h ex- naman
 
-name = input("Enter your name: ")
-for i in range(5,-1,-1):
-    print(name)
+a = input("Enter your name: ")
+b =""
+for i in range(len(a)-1,-1,-1):
+    b += a[i] # to store ulti string in a single line
+if b == a:
+    print("Pallindrome")
+else:
+    print("not a pallindrome")
+    
