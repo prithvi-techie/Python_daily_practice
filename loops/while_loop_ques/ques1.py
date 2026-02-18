6fdc70210d072731e9e6a586b6ab9fb651de63c5
prithvi-techie <prithvi.techie@gmail.com>
2026-02-18 23:59:50 +0530

    #seperate each diit from the numebr and print it on the new line

--- loops/while_loop_ques/ques1.py
+++ loops/while_loop_ques/ques1.py
@@ -1,1 +1,6 @@
 #seperate each diit from the numebr and print it on the new line
+a = 256
+while a>0:    # let a=256 ->  256>0 (next line)
+    print(a%10)   #256%10 -> 6 -> print(6)
+    a = a//10    # 256//10 -> 25.6 -> 25 -> 25>0 (first line) loop start again
+                 # // = floor division, neglect decimal and give only int
