6f4d6df2364874d40efaba5383337ab38c219e8f
prithvi-techie <prithvi.techie@gmail.com>
2026-03-02 19:13:18 +0530

    done 2 ques

---
+++ loops/practice_set/forloop_practice_set1_ans.py
@@ -0,0 +1,15 @@
+# Print numbers from 1 to 20.
+"""for i in range(1,21):
+    print(i)"""
+
+#Print all odd numbers between 1 and 30.
+"""for i in range(1,31):
+    if i%2 !=0:
+        print(i)
+        #or
+a = 1
+for i in range(1,31):
+    if i%2==0:
+        a = a+i
+    else:
+        print(i)"""
