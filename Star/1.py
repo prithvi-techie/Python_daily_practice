6b0d11b639c1bc588b8a64a22f1c666338303787
prithvi-techie <prithvi.techie@gmail.com>
2026-03-05 14:36:50 +0530

    done 3 patterns

---
+++ Star/1.py
@@ -0,0 +1,13 @@
+#
+##
+###
+####
+
+for i in range(1,5):
+    for j in range(1,5):
+        if j<=i:
+            print("*",end="")
+        
+    print()
+    
+    
---
+++ Star/2.py
@@ -0,0 +1,25 @@
+   #
+  ##
+ ###
+####
+for i in range(1,5):
+    for j in range(1,5):
+        if j>=5-i:
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
+    
+
+
+
+
+
+
+
+
+
+"""for i in range(1,5):
+    for j in range(1,5):
+        print("*",end="")
+    print()"""
