32ad9451f285ec4997e121285ffcb218d0f26b47
prithvi-techie <prithvi.techie@gmail.com>
2026-04-15 20:48:45 +0530

    prac

---
+++ practice.py
@@ -0,0 +1,16 @@
+"""
+*
+**
+***
+****
+*****
+
+
+for i in range(5):
+    for j in range(5):
+        if j<=i:
+            print("*",end=" ")
+    else:
+        print() """
+
+# program to sum the digits on number
