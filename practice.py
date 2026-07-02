09bde5dd6e94fb915e9d028e3d97f10560b73e96
prithvi-techie <prithvi.techie@gmail.com>
2026-07-02 17:14:01 +0530

    practice

--- practice.py
+++ practice.py
@@ -13,4 +13,9 @@ for i in range(5):
     else:
         print() """
 
-# program to sum the digits on number
+#sum of n natural number
+n = int(input("Enter the number: "))
+sum = 0
+for i in range(1,n+1):
+    sum+=i
+print("the sum of n natural number : ",sum)
