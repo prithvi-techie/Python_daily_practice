84f9e14eeff38c7ee80071a7ec581ac155a648bb
prithvi-techie <prithvi.techie@gmail.com>
2026-07-05 21:51:55 +0530

    exercise

--- practice.py
+++ practice.py
@@ -1,13 +1,3 @@
-n = int(input("Enter the num: "))
-even = 0
-odd=0
-for i in range(1,n+1):
-    if i%2==0:
-        even=even+i
-    else:
-        odd=odd+i
-print("even sum: ",even)
-print("odd sum: ",odd)
+nums = [1,2,3]
 
-
-        
+print(nums+nums)
