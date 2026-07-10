79a7ba7a186e5f2cccc3617c83d8fc90dd27feb2
prithvi-techie <prithvi.techie@gmail.com>
2026-07-11 00:13:44 +0530

    blahh blahh

--- practice.py
+++ practice.py
@@ -1,7 +1,9 @@
-str = input("enter your name: ")
-
-
-if str==(str[::-1]):
-    print("pallindorme")
+n = int(input("Enter the num: "))
+pallin=0
+while n>0:
+    pallin = n%10
+    n=n//10
+if n == pallin:
+    print("pallindrome")
 else:
     print("not")
